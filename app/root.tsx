9864bb02ceaa6de1b7a18cea04eb164390848de1
samson <samson.omondi1@ibm.com>
2024-12-27 17:51:38 +0300

    setup routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,60 @@
-import { type LinksFunction } from '@remix-run/node'
+import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
-import { LiveReload, Scripts, Links } from '@remix-run/react'
+import { json, type LinksFunction } from '@remix-run/node'
+import {
+	Links,
+	LiveReload,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import { KCDShop } from './kcdshop.tsx'
 import fontStylesheetUrl from './styles/font.css'
-import tailWindStylesheetUrl from './styles/tailwind.css'
-import './styles/global.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
 	return [
 		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
 		{ rel: 'stylesheet', href: fontStylesheetUrl },
-		{ rel: 'stylesheet', href: tailWindStylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
 		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
-	]
+	].filter(Boolean)
+}
+
+export async function loader() {
+	return json({ username: os.userInfo().username })
 }
 
 export default function App() {
+	const data = useLoaderData<typeof loader>()
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
 				<Links />
 			</head>
-			<body>
-				<p className="p-8 text-xl">Hello World</p>
+			<body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
 				<Scripts />
 				<KCDShop />
 				<LiveReload />
