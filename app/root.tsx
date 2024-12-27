1b82f73fef08fbf746e20beb7708b6f7f36c054c
samson <samson.omondi1@ibm.com>
2024-12-27 17:27:26 +0300

    testing bundling css

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,18 @@
 import { type LinksFunction } from '@remix-run/node'
+import { cssBundleHref } from '@remix-run/css-bundle'
 import { LiveReload, Scripts, Links } from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import { KCDShop } from './kcdshop.tsx'
 import fontStylesheetUrl from './styles/font.css'
 import tailWindStylesheetUrl from './styles/tailwind.css'
+import './styles/global.css'
 
 export const links: LinksFunction = () => {
 	return [
 		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
 		{ rel: 'stylesheet', href: fontStylesheetUrl },
 		{ rel: 'stylesheet', href: tailWindStylesheetUrl },
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
 	]
 }
 
