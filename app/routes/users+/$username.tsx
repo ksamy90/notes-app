8e15e6e407ef959d12cb7cfc4c351906649c6767
samson <samson.omondi1@ibm.com>
2025-02-22 15:48:12 +0300

    add data loading features

--- app/routes/users+/$username.tsx
+++ app/routes/users+/$username.tsx
@@ -1,10 +1,25 @@
-import { Link, useParams } from '@remix-run/react'
+import { json, type DataFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData, useParams } from '@remix-run/react'
+import { db } from '#app/utils/db.server.ts'
 
-export default function KodyProfileRoute() {
-	const params = useParams()
+export async function loader({ params }: DataFunctionArgs) {
+	const user = db.user.findFirst({
+		where: {
+			username: {
+				equals: params.username,
+			},
+		},
+	})
+	return json({
+		user: { name: user.name, username: user.username },
+	})
+}
+
+export default function ProfileRoute() {
+	const data = useLoaderData<typeof loader>()
 	return (
-		<div className="container mb-48 mt-36 border-4 border-green-500">
-			<h1 className="text-h1">{params.username}</h1>
+		<div className="container mb-48 mt-3">
+			<h1 className="text-h1">{data.user.name ?? data.user.username}</h1>
 			<Link to="notes" className="underline">
 				Notes
 			</Link>
