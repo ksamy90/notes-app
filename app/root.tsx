4c06ef29874b5f65cde16e1d1fd77c3f8cc1a782
samson <samson.omondi1@ibm.com>
2025-02-22 12:05:59 +0300

    add link tags for notes

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,7 @@ import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
 import { json, type LinksFunction } from '@remix-run/node'
 import {
+	Link,
 	Links,
 	LiveReload,
 	Outlet,
@@ -36,10 +37,12 @@ export default function App() {
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
 				<header className="container mx-auto py-6">
 					<nav className="flex justify-between">
-						<div>
-							<div className="font-light">epic</div>
-							<div className="font-bold">notes</div>
-						</div>
+						<Link to="/">
+							<div>
+								<div className="font-light">epic</div>
+								<div className="font-bold">notes</div>
+							</div>
+						</Link>
 					</nav>
 				</header>
 
@@ -48,10 +51,12 @@ export default function App() {
 				</div>
 
 				<div className="container mx-auto flex justify-between">
-					<div>
-						<div className="font-light">epic</div>
-						<div className="font-bold">notes</div>
-					</div>
+					<Link to="/">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</Link>
 					<p>Built with ♥️ by {data.username}</p>
 				</div>
 				<div className="h-5" />
