31470a72b9e862c48cc6863dd60d77b85940632c
samson <samson.omondi1@ibm.com>
2025-02-23 18:35:26 +0300

    add post feature to notes

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,4 +1,4 @@
-import { json, type DataFunctionArgs } from '@remix-run/node'
+import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
@@ -22,6 +22,19 @@ export async function loader({ params }: DataFunctionArgs) {
 	return json({
 		note: { title: note.title, content: note.content },
 	})
+}
+
+export async function action({ request, params }: DataFunctionArgs) {
+	const formData = await request.formData()
+	const title = formData.get('title')
+	const content = formData.get('content')
+
+	db.note.update({
+		where: { id: { equals: params.noteId } },
+		// @ts-expect-error
+		data: { title, content },
+	})
+	return redirect(`/users/${params.username}/notes/${params.noteId}`)
 }
 
 export default function NoteEdit() {
