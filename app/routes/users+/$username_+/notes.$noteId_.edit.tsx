e2cdb7c4667dd0df6867ec31a4cd9ac5b7a9db27
samson <samson.omondi1@ibm.com>
2025-02-24 15:32:28 +0300

    add pending ui feedback

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,9 +1,15 @@
 import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
-import { Form, useLoaderData } from '@remix-run/react'
+import {
+	Form,
+	useFormAction,
+	useLoaderData,
+	useNavigation,
+} from '@remix-run/react'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { Input } from '#app/components/ui/input.tsx'
 import { Label } from '#app/components/ui/label.tsx'
+import { StatusButton } from '#app/components/ui/status-button.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { db } from '#app/utils/db.server.ts'
 import { invariantResponse } from '#app/utils/misc.tsx'
@@ -40,6 +46,15 @@ export async function action({ request, params }: DataFunctionArgs) {
 
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>()
+
+	//  determine whether this form is submitting
+	const navigation = useNavigation()
+	const formAction = useFormAction()
+	const isSubmitting =
+		navigation.state !== 'idle' &&
+		navigation.formMethod === 'POST' &&
+		navigation.formAction === formAction
+
 	return (
 		<Form
 			method="POST"
@@ -56,7 +71,13 @@ export default function NoteEdit() {
 				<Button variant="destructive" type="reset">
 					Reset
 				</Button>
-				<Button type="submit">Submit</Button>
+				<StatusButton
+					type="submit"
+					disabled={isSubmitting}
+					status={isSubmitting ? 'pending' : 'idle'}
+				>
+					Submit
+				</StatusButton>
 			</div>
 		</Form>
 	)
