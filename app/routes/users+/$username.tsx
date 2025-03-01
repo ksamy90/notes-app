c0d295ec363be7c4fb4967df7959e33ea632840f
samson <samson.omondi1@ibm.com>
2025-03-01 13:49:35 +0300

    add error boundary to user paths

--- app/routes/users+/$username.tsx
+++ app/routes/users+/$username.tsx
@@ -1,5 +1,12 @@
 import { json, type DataFunctionArgs } from '@remix-run/node'
-import { Link, type MetaFunction, useLoaderData } from '@remix-run/react'
+import {
+	isRouteErrorResponse,
+	Link,
+	useLoaderData,
+	useParams,
+	useRouteError,
+	type MetaFunction,
+} from '@remix-run/react'
 import { db } from '#app/utils/db.server.ts'
 import { invariantResponse } from '#app/utils/misc.tsx'
 
@@ -11,7 +18,9 @@ export async function loader({ params }: DataFunctionArgs) {
 			},
 		},
 	})
+
 	invariantResponse(user, 'User not found', { status: 404 })
+
 	return json({
 		user: { name: user.name, username: user.username },
 	})
@@ -20,7 +29,7 @@ export async function loader({ params }: DataFunctionArgs) {
 export default function ProfileRoute() {
 	const data = useLoaderData<typeof loader>()
 	return (
-		<div className="container mb-48 mt-3">
+		<div className="container mb-48 mt-36">
 			<h1 className="text-h1">{data.user.name ?? data.user.username}</h1>
 			<Link to="notes" className="underline" prefetch="intent">
 				Notes
@@ -33,6 +42,27 @@ export const meta: MetaFunction<typeof loader> = ({ data, params }) => {
 	const displayName = data?.user.name ?? params.username
 	return [
 		{ title: `${displayName} | Epic Notes` },
-		{ name: 'description', content: `Profile of ${displayName} on Epic Notes` },
+		{
+			name: 'description',
+			content: `Profile of ${displayName} on Epic Notes`,
+		},
 	]
+}
+
+export function ErrorBoundary() {
+	const error = useRouteError()
+	const params = useParams()
+	console.error(error)
+
+	let errorMessage = <p>Oh no, something went wrong. Sorry about that.</p>
+
+	if (isRouteErrorResponse(error) && error.status === 404) {
+		errorMessage = <p>No user with the username "{params.username}" exists</p>
+	}
+
+	return (
+		<div className="container mx-auto flex h-full w-full items-center justify-center bg-destructive p-20 text-h2 text-destructive-foreground">
+			<p>{errorMessage}</p>
+		</div>
+	)
 }
