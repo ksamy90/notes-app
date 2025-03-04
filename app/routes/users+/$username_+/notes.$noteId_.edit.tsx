d243c54c3b660d9149c0fbb9afae892510924f0d
samson <samson.omondi1@ibm.com>
2025-03-04 23:30:01 +0300

    input validation

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,10 +1,5 @@
 import { json, redirect, type DataFunctionArgs } from '@remix-run/node'
-import {
-	Form,
-	useFormAction,
-	useLoaderData,
-	useNavigation,
-} from '@remix-run/react'
+import { Form, useLoaderData } from '@remix-run/react'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
@@ -14,7 +9,7 @@ import { Label } from '#app/components/ui/label.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { db } from '#app/utils/db.server.ts'
-import { invariantResponse } from '#app/utils/misc.tsx'
+import { invariantResponse, useIsSubmitting } from '#app/utils/misc.tsx'
 export async function loader({ params }: DataFunctionArgs) {
 	const note = db.note.findFirst({
 		where: {
@@ -49,23 +44,30 @@ export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>()
 
 	//  determine whether this form is submitting
-	const navigation = useNavigation()
-	const formAction = useFormAction()
-	const isSubmitting =
-		navigation.state !== 'idle' &&
-		navigation.formMethod === 'POST' &&
-		navigation.formAction === formAction
+	const isSubmitting = useIsSubmitting()
+	const formId = 'note-editor'
 
 	return (
 		<Form
+			id={formId}
 			method="POST"
 			className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
 		>
 			<div className="flex flex-col gap-1">
 				<div>
 					<Label>Title</Label>
-					<Input name="title" defaultValue={data.note.title} />
-					<Textarea name="content" defaultValue={data.note.content} />
+					<Input
+						name="title"
+						defaultValue={data.note.title}
+						required
+						maxLength={100}
+					/>
+					<Textarea
+						name="content"
+						defaultValue={data.note.content}
+						required
+						maxLength={10000}
+					/>
 				</div>
 			</div>
 			<div className={floatingToolbarClassName}>
