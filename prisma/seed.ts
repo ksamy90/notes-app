74c695876976c964dba61025ec40b01ca98cbe81
samson <samson.omondi1@ibm.com>
2025-03-20 16:15:26 +0300

    generate fake data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,61 @@
 import fs from 'node:fs'
+import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const firstNote = await prisma.note.findFirst()
+await prisma.user.deleteMany()
 
-if (!firstNote) {
-	throw new Error('You need to have a note in the database first')
-}
-
-await prisma.note.update({
-	where: { id: firstNote.id },
+await prisma.user.create({
 	data: {
-		images: {
+		email: faker.internet.email(),
+		username: faker.internet.userName(),
+		name: faker.person.fullName(),
+		notes: {
 			create: [
 				{
-					altText: 'an adorable koala cartoon illustration',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/kody-notes/cute-koala.png',
-					),
+					title: faker.lorem.sentence(),
+					content: faker.lorem.paragraphs(),
 				},
 				{
-					altText: 'a cartoon illustration of a koala in a tree eating',
-					contentType: 'image/png',
-					blob: await fs.promises.readFile(
-						'./tests/fixtures/images/kody-notes/koala-eating.png',
-					),
+					title: faker.lorem.sentence(),
+					content: faker.lorem.paragraphs(),
+				},
+			],
+		},
+	},
+})
+
+await prisma.user.create({
+	data: {
+		email: 'kody@kcd.dev',
+		username: 'kody',
+		name: 'Kody',
+		notes: {
+			create: [
+				{
+					id: 'd27a197e',
+					title: 'Basic Koala Facts',
+					content:
+						'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
+					images: {
+						create: [
+							{
+								altText: 'an adorable koala cartoon illustration',
+								contentType: 'image/png',
+								blob: await fs.promises.readFile(
+									'./tests/fixtures/images/kody-notes/cute-koala.png',
+								),
+							},
+							{
+								altText: 'a cartoon illustration of a koala in a tree eating',
+								contentType: 'image/png',
+								blob: await fs.promises.readFile(
+									'./tests/fixtures/images/kody-notes/koala-eating.png',
+								),
+							},
+						],
+					},
 				},
 			],
 		},
