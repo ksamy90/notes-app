412a4e006630e38003999976f388c42d6276705d
samson <samson.omondi1@ibm.com>
2025-03-27 14:30:54 +0300

    link notes to owner

--- app/routes/users+/$username_+/notes.tsx
+++ app/routes/users+/$username_+/notes.tsx
@@ -1,29 +1,24 @@
-import { json, type DataFunctionArgs } from '@remix-run/node'
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
-import { db } from '#app/utils/db.server.ts'
-import { cn, invariantResponse } from '#app/utils/misc.tsx'
+import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
+import { prisma } from '#app/utils/db.server.ts'
+import { cn, getUserImgSrc, invariantResponse } from '#app/utils/misc.tsx'
 
-export async function loader({ params }: DataFunctionArgs) {
-	const owner = db.user.findFirst({
-		where: {
-			username: {
-				equals: params.username,
-			},
+export async function loader({ params }: LoaderFunctionArgs) {
+	const owner = await prisma.user.findUnique({
+		select: {
+			name: true,
+			username: true,
+			image: { select: { id: true } },
+			notes: { select: { id: true, title: true } },
 		},
+		where: { username: params.username },
 	})
+
 	invariantResponse(owner, 'Owner not found', { status: 404 })
-	const notes = db.note
-		.findMany({
-			where: {
-				owner: {
-					username: {
-						equals: params.username,
-					},
-				},
-			},
-		})
-		.map(({ id, title }) => ({ id, title }))
-	return json({ owner, notes })
+
+	//  return the owner.
+	return json({ owner })
 }
 
 export default function NotesRoute() {
@@ -32,21 +27,26 @@ export default function NotesRoute() {
 	const navLinkDefaultClassName =
 		'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl'
 	return (
-		<main className="container flex h-full min-h-[400px] pb-12 px-0 md:px-8">
+		<main className="container flex h-full min-h-[400px] px-0 pb-12 md:px-8">
 			<div className="grid w-full grid-cols-4 bg-muted pl-2 md:container md:mx-2 md:rounded-3xl md:pr-0">
 				<div className="relative col-span-1">
 					<div className="absolute inset-0 flex flex-col">
 						<Link
-							// 🐨 we can get the username from the loader data instead
 							to={`/users/${data.owner.username}`}
-							className="pb-4 pl-8 pr-4 pt-12"
+							className="flex flex-col items-center justify-center gap-2 bg-muted pb-4 pl-8 pr-4 pt-12 lg:flex-row lg:justify-start lg:gap-4"
 						>
-							<h1 className="text-base font-bold md:text-lg lg:text-left lg:text-2xl">
+							<img
+								src={getUserImgSrc(data.owner.image?.id)}
+								alt={ownerDisplayName}
+								className="h-16 w-16 rounded-full object-cover lg:h-24 lg:w-24"
+							/>
+							<h1 className="text-center text-base font-bold md:text-lg lg:text-left lg:text-2xl">
 								{ownerDisplayName}'s Notes
 							</h1>
 						</Link>
 						<ul className="overflow-y-auto overflow-x-hidden pb-12">
-							{data.notes.map(note => (
+							{/* 🐨 this will need to be updated since the notes are on data.owner.notes now */}
+							{data.owner.notes.map(note => (
 								<li key={note.id} className="p-1 pr-0">
 									<NavLink
 										to={note.id}
@@ -68,5 +68,17 @@ export default function NotesRoute() {
 				</div>
 			</div>
 		</main>
+	)
+}
+
+export function ErrorBoundary() {
+	return (
+		<GeneralErrorBoundary
+			statusHandlers={{
+				404: ({ params }) => (
+					<p>No user with the username "{params.username}" exists</p>
+				),
+			}}
+		/>
 	)
 }
