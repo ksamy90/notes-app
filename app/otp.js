675101e6f1c57242315b100a3c50c8da05a333f0
samson <samson.omondi1@ibm.com>
2025-05-06 19:21:23 +0300

    otp test to generate code

---
+++ app/otp.js
@@ -0,0 +1,17 @@
+import { generateTOTP } from '@epic-web/totp'
+
+const otpUri = new URL(
+	'otpauth://totp/localhost%3A3000:kody%40example.dev?secret=3MJXYZC62C4FKU43&issuer=localhost%3A3000&algorithm=SHA1&digits=6&period=30',
+)
+const { secret, algorithm, digits, period } = Object.fromEntries(
+	otpUri.searchParams.entries(),
+)
+
+const { otp } = generateTOTP({
+	secret,
+	algorithm,
+	digits,
+	period,
+})
+
+console.log(otp)
