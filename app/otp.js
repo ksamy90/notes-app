5048a8026009f37b8d6eb83bafea59c2746f8f40
samson <samson.omondi1@ibm.com>
2025-05-09 16:55:04 +0300

    use disable 2FA in disable.tsx

--- app/otp.js
+++ app/otp.js
@@ -1,7 +1,7 @@
 import { generateTOTP } from '@epic-web/totp'
 
 const otpUri = new URL(
-	'otpauth://totp/localhost%3A3000:kody%40kcd.dev?secret=LG7W2IV3HBO2ZWES&issuer=localhost%3A3000&algorithm=SHA1&digits=6&period=30',
+	'otpauth://totp/localhost%3A3000:kody%40kcd.dev?secret=EYK32Z76PS4OERYX&issuer=localhost%3A3000&algorithm=SHA1&digits=6&period=30',
 )
 const { secret, algorithm, digits, period } = Object.fromEntries(
 	otpUri.searchParams.entries(),
--- app/routes/settings+/profile.two-factor.disable.tsx
+++ app/routes/settings+/profile.two-factor.disable.tsx
@@ -12,30 +12,64 @@ import { validateCSRF } from '#app/utils/csrf.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { useDoubleCheck, useIsPending } from '#app/utils/misc.tsx'
 import { redirectWithToast } from '#app/utils/toast.server.ts'
+import { shouldRequestTwoFA } from '../_auth+/login.tsx'
+import { getRedirectToUrl } from '../_auth+/verify.tsx'
 import { twoFAVerificationType } from './profile.two-factor.tsx'
 
 export const handle = {
 	breadcrumb: <Icon name="lock-open-1">Disable</Icon>,
 }
 
+// 🐨 create a "requireRecentVerification" function that accepts a request and the userId
+// 🐨 call shouldRequestTwoFA with the request and userId
+// 🐨 if we should reverify, then get a verification URL with getRedirectToUrl
+// from '#app/routes/_auth+/verify.tsx'
+// 🐨 redirect to that URL
+// 💯 as a bonus, use redirectWithToast and let the user know why they're being
+// required to reverify.
+export async function requireRecentVerification({
+	request,
+	userId,
+}: {
+	request: Request
+	userId: string
+}) {
+	const shouldVerify = await shouldRequestTwoFA({ request, userId })
+	if (shouldVerify) {
+		const reqUrl = new URL(request.url)
+		const redirectUrl = getRedirectToUrl({
+			request,
+			target: userId,
+			type: twoFAVerificationType,
+			redirectTo: reqUrl.pathname + reqUrl.search,
+		})
+		throw await redirectWithToast(redirectUrl.toString(), {
+			title: 'Please Reverify',
+			description: 'Please reverify your account before proceeding',
+		})
+	}
+}
+
 export async function loader({ request }: LoaderFunctionArgs) {
 	await requireUserId(request)
+	// 🐨 call requireRecentVerification with the request and userId (from requireUserId)
+	await requireRecentVerification({
+		request,
+		userId: await requireUserId(request),
+	})
 	return json({})
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-	// 🐨 get the userId from this:
 	const userId = await requireUserId(request)
+	// 🐨 call requireRecentVerification with the request and userId
+	await requireRecentVerification({ request, userId })
 	const formData = await request.formData()
 	await validateCSRF(formData, request.headers)
-
-	// 🐨 delete the user's twoFAVerificationType verification from the database
 	await prisma.verification.delete({
 		where: { target_type: { target: userId, type: twoFAVerificationType } },
 	})
-
 	throw await redirectWithToast('/settings/profile/two-factor', {
-		// 🐨 fix this title and description
 		title: '2FA Disabled',
 		description: 'Two factor authentication has been disabled.',
 	})
