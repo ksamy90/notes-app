cc0590b5caa9c355d429cb0c1366ac7f12331759
samson <samson.omondi1@ibm.com>
2025-05-12 20:09:27 +0300

    include variables for auth utils

---
+++ app/routes/_auth+/auth.$provider.ts
@@ -0,0 +1,17 @@
+import { redirect, type ActionFunctionArgs } from '@remix-run/node'
+import { authenticator } from '#app/utils/auth.server.ts'
+import { handleMockAction } from '#app/utils/connections.server.ts'
+import { ProviderNameSchema } from '#app/utils/connections.tsx'
+
+export async function loader() {
+	return redirect('/login')
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+	const providerName = ProviderNameSchema.parse(params.provider)
+	console.log(params)
+
+	await handleMockAction(providerName, request)
+
+	return await authenticator.authenticate(providerName, request)
+}
